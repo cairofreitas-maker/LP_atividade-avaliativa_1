244d05e2731a06cee10047df0a0ae48863ff67bb
cairofreitas-maker <cairo.freitas@ba.estudante.senai.br>
2026-03-11 16:47:00 -0300

    Add files via upload

---
+++ for_number.py
@@ -0,0 +1,12 @@
+import os
+os.system("cls")
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+n3 = int(input("Digite o terceiro número: "))
+n4 = int(input("Digite o quarto número: "))
+n5 = int(input("Digite o quinto número: "))
+
+soma = n1 + n2 + n3 + n4 + n5
+
+print("Soma:", soma)
